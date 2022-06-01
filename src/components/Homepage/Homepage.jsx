fad0f64eae021a11edd2e3a8eb9b3164a8f7372a
Roman Medvedev <roman.medvedev77@gmail.com>
2022-05-31 23:26:51 -0400

    add movielist, movie card components

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
+import MovieList from '../RenderMovies/MovieList';
+import getData from '../../utilities/getData';
 
 function Homepage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getData().then((res) => setMovies(res));
+    // console.log('movies', movies);
+  }, []);
   return (
     <div>
       Homepage
       <SearchBar />
+      <MovieList movies={movies} />
     </div>
   );
 }
---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,10 @@
+function MovieCard({ movie }) {
+  return (
+    <div>
+      MovieCard
+      <div>{movie.id}</div>
+    </div>
+  );
+}
+
+export default MovieCard;
