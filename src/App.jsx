f150fa56de6af87027af7dd8c60501846b4c4e4d
Roman Medvedev <roman.medvedev77@gmail.com>
2022-06-14 11:07:26 -0400

    add header router, integrete it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+
+import Header from './routes/header/Header';
 import Homepage from './routes/homepage/Homepage';
 import Search from './routes/search/Search';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/search/*" element={<Search />} />
+      <Route path="/" element={<Header />}>
+        <Route index element={<Homepage />} />
+        <Route path="/search/*" element={<Search />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/routes/header/Header.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      Header
+      <Outlet />
+    </div>
+  );
+}
+
+export default Header;
