a06c30b08c1fbeb224cea6f1cb6377b49e8d3d49
Roman Medvedev <roman.medvedev77@gmail.com>
2022-07-19 15:38:35 -0400

    change css style layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import NavBar from './components/Navbar/Navbar';
 import Homepage from './pages/home/Home';
 import Search from './pages/search/Search';
 import Movies from './pages/Movies/Movies';
+import Footer from './pages/Footer/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path={route.HOME} element={<NavBar />}>
-        <Route index element={<Homepage />} />
-        <Route path={route.SEARCH} element={<Search />} />
-        <Route path={route.MOVIES} element={<Movies />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path={route.HOME} element={<NavBar />}>
+          <Route index element={<Homepage />} />
+          <Route path={route.SEARCH} element={<Search />} />
+          <Route path={route.MOVIES} element={<Movies />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
