ab5e5604dd42612b74ae0de3bb29ed4c59b55ffd
Roman Medvedev <roman.medvedev77@gmail.com>
2022-08-22 23:19:50 -0400

    add styled component for movies preview

--- src/pages/MoviesPreview/MoviesPreview.jsx
+++ src/pages/MoviesPreview/MoviesPreview.jsx
@@ -1,29 +1,41 @@
 /* eslint-disable object-curly-newline */
 import { useEffect, useState } from 'react';
-import { Icon, Pagination, Dimmer, Loader } from 'semantic-ui-react';
-import MovieList from '../../components/MovieList/MovieList';
+import { useNavigate } from 'react-router-dom';
+
+import { Icon, Pagination, Dimmer, Loader, Card } from 'semantic-ui-react';
+// import MovieList from '../../components/MovieList/MovieList';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 import getData from '../../utilities/getData';
 
-import { popularMovieUrl } from '../../constants/apiUrls';
+import { popularMovieUrl, TMDB_POSTER_PATH } from '../../constants/apiUrls';
+
+import {
+  StyledMoviesContainer,
+  StyledMoviesList,
+  StyledPaginationWrapper
+} from './moviesPreview.style';
 
 function MoviesPreview() {
   const [isLoading, setIsLoading] = useState(false);
   const [moviesPreview, setMoviespreview] = useState(null);
   const [activePage, setActivePage] = useState(1);
 
+  const navigate = useNavigate();
+  const cardHandleClick = (category, id) => {
+    navigate(`/${category}/${id}`);
+  };
+
   useEffect(() => {
     const loadMovies = async () => {
       setIsLoading(true);
       const movieUrl = popularMovieUrl(activePage);
       const res = await getData(movieUrl);
-      console.log('res', res);
-      setMoviespreview(res);
+      setMoviespreview(res.results);
 
       setTimeout(() => {
         setIsLoading(false);
-      }, 400);
+      }, 500);
     };
     loadMovies();
   }, [activePage]);
@@ -33,32 +45,52 @@ function MoviesPreview() {
   };
 
   return (
-    <div>
-      Search
-      <div>
-        <SearchBar />
-      </div>
-      {isLoading && (
-        <Dimmer active>
-          <Loader size="medium">Loading...</Loader>
-        </Dimmer>
-      )}
-      {moviesPreview.results && (
-        <>
-          <MovieList movies={moviesPreview.results} category="movie" />
-          <Pagination
-            defaultActivePage={activePage}
-            ellipsisItem={{ content: <Icon name="ellipsis horizontal" />, icon: true }}
-            firstItem={{ content: <Icon name="angle double left" />, icon: true }}
-            lastItem={{ content: <Icon name="angle double right" />, icon: true }}
-            prevItem={{ content: <Icon name="angle left" />, icon: true }}
-            nextItem={{ content: <Icon name="angle right" />, icon: true }}
-            totalPages={moviesPreview.total_pages}
-            onPageChange={onChange}
-          />
-        </>
-      )}
-    </div>
+    <StyledMoviesContainer>
+      <StyledMoviesList>
+        <div>
+          <SearchBar />
+        </div>
+        {isLoading && (
+          <Dimmer active>
+            <Loader size="medium">Loading...</Loader>
+          </Dimmer>
+        )}
+        {moviesPreview && (
+          <>
+            {/* <MovieList movies={moviesPreview} category="movie" /> */}
+            <h1>Popular Movies</h1>
+            <Card.Group itemsPerRow={5}>
+              {moviesPreview.map((movie) => (
+                <Card
+                  key={movie.id}
+                  image={
+                    movie.backdrop_path
+                      ? `${TMDB_POSTER_PATH + movie.poster_path}`
+                      : 'https://react.semantic-ui.com/images/wireframe/image.png'
+                  }
+                  header={movie.title}
+                  meta={movie.release_date}
+                  onClick={() => cardHandleClick('movie', movie.id)}
+                />
+              ))}
+            </Card.Group>
+            <StyledPaginationWrapper>
+              <Pagination
+                defaultActivePage={activePage}
+                ellipsisItem={{ content: <Icon name="ellipsis horizontal" />, icon: true }}
+                firstItem={{ content: <Icon name="angle double left" />, icon: true }}
+                lastItem={{ content: <Icon name="angle double right" />, icon: true }}
+                prevItem={{ content: <Icon name="angle left" />, icon: true }}
+                nextItem={{ content: <Icon name="angle right" />, icon: true }}
+                totalPages={500}
+                onPageChange={onChange}
+                inverted
+              />
+            </StyledPaginationWrapper>
+          </>
+        )}
+      </StyledMoviesList>
+    </StyledMoviesContainer>
   );
 }
 
---
+++ src/pages/MoviesPreview/moviesPreview.style.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const StyledMoviesContainer = styled.main`
+  color: #fff;
+  max-width: 1172px;
+  margin: auto;
+  font-family: 'Mulish', sans-serif;
+  h1 {
+    font-weight: 800;
+    font-size: 2.5em;
+    margin: 2.5em 0;
+  }
+`;
+
+export const StyledMoviesList = styled.section`
+  /* width: 100%; */
+  // img background full height
+  /* height: 100vh; */
+  /* display: flex; */
+  margin-top: 4.8em;
+`;
+
+export const StyledPaginationWrapper = styled.div`
+  margin-top: 4.5em;
+  display: block;
+  text-align: center;
+`;
