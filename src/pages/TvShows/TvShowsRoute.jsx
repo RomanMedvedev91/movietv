9b7615dea31e7f4102e48ce0a94903cf8884964d
Roman Medvedev <roman.medvedev77@gmail.com>
2022-09-02 13:58:14 -0400

    add seasons pages, add season routes in tv routes

--- src/pages/TvShows/TvShowsRoute.jsx
+++ src/pages/TvShows/TvShowsRoute.jsx
@@ -6,6 +6,8 @@ import TvShowsPopular from './TvShowsPopular';
 import TvShowsAiringToday from './TvShowsAiringToday';
 import TvShowsOnTheAir from './TvShowsOnTheAir';
 import TvShowsTopRated from './TvShowsTopRated';
+import Seasons from '../Seasons/Seasons';
+import Season from '../Season/Season';
 
 function TvShowsRoute() {
   return (
@@ -14,7 +16,14 @@ function TvShowsRoute() {
       <Route path={route.AIRING_TODAY} element={<TvShowsAiringToday />} />
       <Route path={route.ON_TV} element={<TvShowsOnTheAir />} />
       <Route path={route.TOP_RATED} element={<TvShowsTopRated />} />
-      <Route path={route.TVSHOW_ID} element={<TvShow />} />
+      <Route path={route.TVSHOW_ID}>
+        <Route index element={<TvShow />} />
+        <Route path={route.TVSHOW_SEASONS} element={<Seasons />} />
+
+        <Route path={route.TVSHOW_SEASON}>
+          <Route path={route.TVSHOW_SEASON_ID} element={<Season />} />
+        </Route>
+      </Route>
     </Routes>
   );
 }
